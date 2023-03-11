a94b21ed1942cf50877fb2cfda369fb1bee28142
codewithfahmi <fahmiyahya0@gmail.com>
2023-03-11 22:13:54 +0700

    Add spectre.css to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Beranda</title>
+</svelte:head>
+
+<main>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis numquam harum est ipsum voluptatibus exercitationem laudantium perferendis pariatur! Ratione error totam porro consectetur quibusdam dolores tenetur facere rerum voluptatibus quidem.
+    </p>
+</main>
